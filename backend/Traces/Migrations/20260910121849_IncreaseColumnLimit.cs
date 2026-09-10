dbccdede0a5a8bedfefbb583299c37a53802b061
LeviOS31 <58821386+LeviOS31@users.noreply.github.com>
2026-09-10 14:20:24 +0200

    Fixed column length issue for spanattributes

---
+++ backend/Traces/Migrations/20260910121849_IncreaseColumnLimit.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Traces.Migrations
+{
+    /// <inheritdoc />
+    public partial class IncreaseColumnLimit : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}

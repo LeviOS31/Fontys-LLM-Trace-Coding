cd047ab0988646f18e2459f0acdaaa15747dbc1e
LeviOS31 <58821386+LeviOS31@users.noreply.github.com>
2026-09-11 13:51:08 +0200

    added PGadmin to docker file

---
+++ backend/Traces/Migrations/20260911114742_ReaddedTraceGroups.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Traces.Migrations
+{
+    /// <inheritdoc />
+    public partial class ReaddedTraceGroups : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
